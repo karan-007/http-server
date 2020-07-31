c2f7d8b72619caf676b31a7f227470de392649c6
Karan Chouhan <karan.chouhan@mountblue.tech>
2020-07-31 10:38:55 +0530

    refactored using switch case

--- index.js
+++ index.js
@@ -1,49 +1,73 @@
-var http = require("http");
-var fs = require("fs");
-var url = require("url");
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 const { v4: uuidv4 } = require("uuid");
 
-var server = http.createServer(function (req, res) {
-  var path = url.parse(req.url).pathname;
-  if (req.url === "/html") {
-    fs.readFile("./index.html", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url === "/json") {
-    fs.readFile("./data.json", function (err, data) {
-      res.writeHead(200, { "Content-Type": "json" });
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url === "/uuid") {
-    res.writeHead(200, { "Content-Type": "json" });
-    res.write(uuidv4());
-    res.end();
-  } else if (req.url === path) {
-    let key = String(path);
-    key = key.split("/");
+const server = http.createServer(function (req, res) {
+  const path = url.parse(req.url).pathname;
+  let key = String(path);
+  key = key.split("/");
 
-    if (key[1] === "delay") {
+  switch (path) {
+    case "/":
+      fs.readFile("./home.html", function (err, data) {
+        if (err) {
+          res.write("failed to read");
+          res.end();
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+        }
+      });
+      break;
+
+    case "/html":
+      fs.readFile("./index.html", function (err, data) {
+        if (err) {
+          res.write("failed to read");
+          res.end();
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+        }
+      });
+      break;
+
+    case "/json":
+      fs.readFile("./data.json", function (err, data) {
+        res.writeHead(200, { "Content-Type": "json" });
+        res.write(data);
+        res.end();
+      });
+      break;
+
+    case "/uuid":
+      res.writeHead(200, { "Content-Type": "json" });
+      res.write(uuidv4());
+      res.end();
+      break;
+
+    case "/delay/" + key[2]:
       setTimeout(() => {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write("Hey you are " + key[2] + " seconds delayed");
         res.end();
       }, parseInt(key[2]) * 1000);
-    } else if (key[1] === "status") {
+      break;
+
+    case "/status/" + key[2]:
       res.writeHead(parseInt(key[2]), { "Content-Type": "text/html" });
       res.write("Status code is " + key[2]);
       res.end();
-    } else {
+      break;
+
+    default:
       res.writeHead(404);
       res.write("opps this doesn't exist - 404");
       res.end();
-    }
-  } else {
-    res.writeHead(404);
-    res.write("opps this doesn't exist - 404");
-    res.end();
+      break;
   }
 });
 
